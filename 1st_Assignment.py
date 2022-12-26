1cd87f79185d6ead6a8591325a767dac58e7ab1f
Hriday Jyoti Ghosh <108257192+hriday-ghosh@users.noreply.github.com>
2022-12-26 20:51:00 +0530

    Add files via upload

---
+++ 1st_Assignment.py
@@ -0,0 +1,55 @@
+"""
+Ineuron Assignment_No. 1
+Date- 26/12/2022
+
+1. In the below elements which of them are values or an expression? eg:- values can be integer or string and expressions will be mathematical operators.
+Ans- 
+'hello' is a expression
+-87.8 - value
+-  - expression 
+/ - expression
++	- expression	
+6  - value
+
+2. What is the difference between string and variable?
+A string is a sequence of one or more characters (letters, numbers, symbols) that can be either a constant or a variable.
+Variables are symbols that you can use to store data in a program.
+
+3. Describe three different data types.
+There are three type of data types 
+1. Int , 2. Boolean, 3. Float and 4. String.
+4. What is an expression made up of? What do all expressions do?
+Ans- A combination of operands and operators is called an expression. 
+The +, -, and * operators for mathematical addition, subtraction, and multiplication are similar to other languages, but the behaviour of division differs. There are two types of divisions in Python: floor division (or integer division) // and floating-point/division.[93] Python uses the ** operator for exponentiation. We have many expressions as well.
+5. This assignment statements, like spam = 10. What is the difference between an expression and a statement?
+Ans- Expressions are unique in meaning, while statements are two-sided in execution and Statements are the whole structure. 
+Spam = 10
+Spam = 10 is a expression and 
+The assignment statement (=) binds a name as a reference to a separate, dynamically-allocated object.
+Here, Spam’s values is 10.
+        
+6. After running the following code, what does the variable bacon contain?
+bacon = 22
+bacon + 1
+Ans = 23
+7. What should the values of the following two terms be?
+'spam' + 'spamspam'
+'spam' * 3
+Ans- spamspamspam
+
+8. Why is eggs a valid variable name while 100 is invalid?
+Ans- Cause, 100 is a number and variable names cannot begin with a number.
+
+9. What three functions can be used to get the integer, floating-point number, or string version of a value?
+Ans- Use int() for integer value ,
+Use float() for float value, and 
+Use str() for string.
+
+10. Why does this expression cause an error? How can you fix it?
+'I have eaten ' + 99 + ' burritos.'
+Ans- We cant concatenate or add in this above way we can simply right 'I have eaten ' + '99' + ' burritos.' 
+
+
+
+
+"""
